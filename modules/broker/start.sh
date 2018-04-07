a24d30b13ebeed15022de80c9f1abb031f641456
Brice Copy <brice.copy@cern.ch>
2018-04-07 23:11:16 +0200

    Add sample activemq config and update startup script

--- modules/broker/start.sh
+++ modules/broker/start.sh
@@ -1,2 +1,1 @@
-# -v `pwd`/src/main/docker/conf:/opt/activemq/conf
-docker run -it --rm --name=activemq -p 1883:1883 -p 61616:61616 -p 8161:8161  webcenter/activemq
+docker run -it --rm -v `pwd`/src/main/docker/conf/activemq.xml:/opt/activemq/conf/activemq.xml:z --name=activemq -p 1883:1883 -p 61616:61616 -p 8161:8161  webcenter/activemq
